ef91ec25b9844b857f6fd3e1da9e7a7657d4c687
Zhenhua <zhenhua1109@yahoo.com>
2022-11-02 15:22:56 -0700

    add arrayDiff

---
+++ arrayDiff.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Implement a difference function, which subtracts one list from another and returns the result.
+// It should remove all values from list a, which are present in list b keeping their order.
+
+function arrayDiff(a, b) {
+  return a.filter(val => !b.includes(val));
+}
